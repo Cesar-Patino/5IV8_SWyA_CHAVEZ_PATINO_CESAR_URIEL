de817d213758486aab643f0c4321149019ecfa15
unknown <cchavezp1900@alumno.ipn.mx>
2021-10-07 01:29:01 -0500

    por fin

---
+++ cifradoDES/src/cifradodes/CifradoDES.java
@@ -0,0 +1,20 @@
+/*
+ CHAVEZ PATIÑO CESAR URIEL 5IV8
+ */
+package cifradodes;
+
+/**
+ *
+ * @author CHAVEZ
+ */
+public class CifradoDES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        menu ci = new menu();
+        ci.setVisible(true);
+    }
+}
